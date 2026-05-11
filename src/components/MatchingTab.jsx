5977c1dc2aeb067daedc570d4431c2cf4bc41076
Jester Lloyd Bautista <jesterlloyd@Jesters-MacBook-Pro.local>
2026-05-11 01:30:41 -0700

    Fix Embed tab crash

--- src/components/MatchingTab.jsx
+++ src/components/MatchingTab.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import EmbedUnitCard from './EmbedUnitCard'
 import StudentMatchCard from './StudentMatchCard'
 import UnitSetupPanel from './UnitSetupPanel'
